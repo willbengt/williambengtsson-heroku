1ba051c1f81c3c7ba100502d661ea432e22f2d71
willbengt <wilbe075@student.liu.se>
2017-04-17 14:55:40 +0200

    Migrated all files from gcloud app

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, send_file, request, jsonify, render_template
+#from flask_cors import CORS
+from monkeylearn import MonkeyLearn
+from flask_api import FlaskAPI
+import json
 
-@app.route('/')
-def hello_world():
-    return 'Hello!'
+app = FlaskAPI(__name__)
+#app = Flask(__name__)
+#CORS(app)
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/")
+def index():
+    return send_file('templates/index.html')
+
+@app.route('/getData/')
+def getData():
+  return {'name':'roy'}
+
+# @app.route('/Authenticate',methods=['POST'])
+# def Authenticate():
+#     content = request.json
+#     return jsonify({'username':content['username'],'password':content['password']})
+
+# @app.route('/home')
+# def home():
+#   bla = 'blah..blah...'
+#   return render_template('home.html', bla=bla)
+
+@app.route("/ml")
+def ml():
+  ml = MonkeyLearn("f0662751f5792b646356ec5516580c239b71aefb")
+  text_list = ["Disney Pixar Cars Toon: Mater's Tall tales - Walmart (Wii) Cars Toon: Mater's Tall Tales [Disney Pixar], a Nintendo console exclusive, is a comedy themed mini-game(s) / party game with action racing elements."]
+  module_id = 'cl_oFKL5wft'
+  res = ml.classifiers.classify(module_id, text_list, sandbox=True)
+  list1 = res.result
+  str1 = ''.join(str(e) for e in list1)
+  str2 = str1.rstrip('\n')
+  str3 = str2.replace("u'", "'")
+  str4 = str3.replace("'", '"')
+  parsed = json.loads(str4)
+  return parsed
+
+
+if __name__=="__main__":
+  app.run(debug=True)
---
+++ static/js/app.js
@@ -0,0 +1,49 @@
+'use strict';   // See note about 'use strict'; below
+
+var wbApp = angular.module('wbApp', [
+ 'ngRoute',
+]);
+
+wbApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: '/static/partials/index.html',
+            }).
+            when('/about', {
+                templateUrl: '../static/partials/about.html',
+                controller: 'AboutCtrl'
+            }).
+            when('/home', {
+                templateUrl: '../templates/home.html',
+                controller: 'HomeCtrl'
+            }).
+            otherwise({
+                 redirectTo: '/'
+            });
+    }]);
+
+wbApp.controller('AboutCtrl', ['$http','$scope',function($http,$scope) {
+
+    $scope.loading = false;
+
+    //$scope.callData = function($event){
+    $scope.callData = function(){
+        //$event.preventDefault();
+        $scope.loading = true;
+        $http({
+            method:'GET',
+            url:'/ml',
+            headers: {
+               'Content-Type': 'application/json;charset=utf-8'
+            }
+        })
+        .then(function(resp){
+            $scope.results = resp.data;
+        },function(error){
+            console.log(error);
+        }).finally(function() {
+            $scope.loading = false;
+        });
+    }
+}]);
